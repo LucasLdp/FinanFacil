1f3f5238d4910a89cddf59a2cd823f74a93d5690
LucasLdp <lucasdepauloldp1@gmail.com>
2025-08-09 00:18:28 -0300

    feat: replace default content with login form and improve UI layout

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,39 +1,72 @@
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { PasswordInput } from '@/components/ui/password-input'
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { useForm } from 'react-hook-form'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+  const form = useForm()
+  const { control } = form
+
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
+    <main className="h-screen flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <h4 className="font-bold text-2xl">Economize com o FinanFácil 💸</h4>
+        <span>Insira suas credenciais para entrar na sua conta</span>
+        <Form {...form}>
+          <form className="w-full flex flex-col gap-4" action="">
+            <FormField
+              control={control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Insira seu email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <PasswordInput placeholder="Insira sua senha" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              className="bg-green-600 hover:bg-green-900 cursor-pointer"
+              type="submit"
+            >
+              Entrar
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </main>
   )
 }

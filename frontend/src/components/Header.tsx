41d6234f3cb64d82d29769ec4d015aebb594a878
LucasLdp <lucasdepauloldp1@gmail.com>
2025-08-09 00:19:29 -0300

    refactor: simplify Header component by removing navigation and adding logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,9 @@
-import { Link } from '@tanstack/react-router'
+import logo from '@/assets/logo.svg'
 
 export default function Header() {
   return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
-        </div>
-      </nav>
+    <header className="px-22 py-6 flex gap-2 bg-white text-black justify-between">
+      <img src={logo} alt="Logo" />
     </header>
   )
 }
